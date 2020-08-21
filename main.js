8a188d1017bed393f884962201be9cae9d89e313
HaoTran <31514669+donxiya@users.noreply.github.com>
2020-08-21 17:07:29 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+//8-bit reverse
+function revers8bit(int){
+    let binaryArr = (int >>> 0).toString(2).split("");
+    let paddedArr = [0, 0].concat(result).reverse().join("");
+    let result = parseInt((result2 + '').replace(/[^01]/gi, ''), 2);
+    return result
+}
